156ea015ef44cc8608a539d076c240918a53e949
TabrezHira <51405264+Tabrezhira@users.noreply.github.com>
2025-06-06 23:13:51 +0530

    updateOrderStatus Api Done

--- SERVER/src/controllers/order/order.js
+++ SERVER/src/controllers/order/order.js
@@ -32,9 +32,50 @@ export const createOrder = async(req,reply) => {
                 address:branchData.address || 'No address available'
             }
         })
-        
+        const saveOrder = await newOrder.save()
+        return reply.status(201).send(saveOrder)
     } catch (error) {
-        console.log(err);
+        console.log(error);
         return reply.status(500).send({message:'Failed to create order', error})
     }
+}
+
+export const confirmOrder = async(req, reply) => {
+    try {
+        const {orderId} = req.params;
+        const {userId} = req.user;
+        const {deliveryPersonLocation} = req.body;
+
+        const deliveryPerson = await DeliveryPartner.findById(userId);
+        if(!deliveryPerson){
+            return reply.status(404).send({message:'Delivery Person not found'})
+        }
+        const order = await Order.findById(orderId);
+        if(!order) return reply.status(404).send({message:'Order not found'})
+        
+        if (order.status !== 'available'){
+            return reply.status(400).send({message:'Order is not available'})
+        }
+
+        order.status = 'confirmed';
+
+        order.deliveryPartner = userId;
+        order.deliveryPersonLocation = {
+            latitude: deliveryPersonLocation?.latitude,
+            longitude:deliveryPersonLocation?.longitude,
+            address: deliveryPersonLocation.address || '',
+        };
+
+        req.server.io.to(orderId).emit('orderConfirmed', order)
+        await order.save()
+
+        return reply.send(order)
+    } catch (error) {
+        console.log(error);
+        return reply.status(500).send({message:'Failed to confirmOrder', error})
+    }
+}
+
+export const updateOrderStatus = async(req, reply) => {
+
 }
