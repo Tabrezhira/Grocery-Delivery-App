49018e3da5f30fc77dac348e445bb49916cf0bfe
TabrezHira <51405264+Tabrezhira@users.noreply.github.com>
2025-06-18 22:39:52 +0530

    start

--- SERVER/src/config/setup.js
+++ SERVER/src/config/setup.js
@@ -4,5 +4,68 @@ import * as AdminJSMongoose from '@adminjs/mongoose'
 import fastifySession from '@fastify/session'
 import fastifyCookie from '@fastify/cookie'
 import * as Models from '../models/index.js' // Your Mongoose models
-import { authenticate, COOKIE_PASSWORD } from './config.js'
+import { authenticate, COOKIE_PASSWORD, sessionStore } from './config.js'
 import { dark, light, noSidebar } from '@adminjs/themes'
+
+
+AdminJS.registerAdapter(MongooseAdapter)
+
+export const admin = new AdminJS({
+    resources:[
+        {
+            resource:Models.Customer,
+            Option:{
+                listProperties:["phone",'role','isActivated'],
+                filterProperties:['phone','role']
+            },
+
+        },{
+
+            resource:Models.DeliveryPartner,
+            Option:{
+                listProperties:["email","role","isActivated"],
+                filterProperties:["email","role"],
+            },
+        },
+        {
+            resource:Models.Admin,
+            Option:{
+                listProperties:["email","role","isActivated"],
+                filterProperties:["email","role"],
+            }
+
+        },
+        {resource:Models.Branch},
+        {resource:Models.Product},
+        {resource:Models.Category},
+        {resource:Models.Order},
+        {resource:Models.Counter},
+    ],
+    branding:{
+        companyName:'Grocery Delivery App',
+        withMadeWithLove:false,
+    },
+    defaultTheme:dark.id,
+    availableThemes:[dark,light,noSidebar],
+    rootPath:'/admin'
+})
+
+export const buildAdminRouter = async(app) => {
+    await AdminJSFastify.buildAuthenticatedRouter(
+        admin,{
+            authenticate,
+            cookiePassword:COOKIE_PASSWORD,
+            cookieName:'adminJs'
+        },
+        app,
+        {
+            store:sessionStore,
+            saveUninitialized: true,
+            secret: COOKIE_PASSWORD,
+            cookie:{
+                httpOnly:process.env.NODE_ENV === 'production',
+                secure:process.env.NODE_ENV === 'production',
+            }
+        }
+    )
+}
